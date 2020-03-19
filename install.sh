ee179a13de1db744afde335f033ac8b0f4fc4190
Georgi Sabev <gcapizzi@pivotal.io>
2020-03-19 12:32:23 +0200

    Make install script more robust
    Signed-off-by: Danail Branekov <danailster@gmail.com>
    Co-authored-by: Danail Branekov <danailster@gmail.com>

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-stow --target "$HOME" nvim tmux zsh
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+stow --dir="$SCRIPT_DIR" --target "$HOME" nvim tmux zsh
