dc59d28a3a8c232061bb0fdb96ddd133a0112199
skupnjak <skupnjak@de.ibm.com@skupnjaks-MacBook-Pro.local>
2020-04-02 16:52:47 +0200

    add git to install script

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-stow --dir="$SCRIPT_DIR" --target "$HOME" nvim tmux zsh
+stow --dir="$SCRIPT_DIR" --target "$HOME" nvim tmux zsh git
