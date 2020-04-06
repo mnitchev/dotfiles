4a377df32b61557bd4d8383cd34a44bd573d44ab
Georgi Sabev <georgethebeatle@gmail.com>
2020-04-06 11:33:32 +0000

    Generate .gitconfig and include the common config

--- install.sh
+++ install.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
+set -xeuo pipefail
 
+NAME=${NAME:-"Anonymous Eirininaut"}
+EMAIL=${EMAIL:-"eirini@cloudfoundry.org"}
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-stow --dir="$SCRIPT_DIR" --target "$HOME" nvim tmux zsh git
+main() {
+  generate_gitconfig
+  configure_home
+}
+
+generate_gitconfig() {
+  cat <<EOF > "$HOME/.gitconfig"
+[include]
+  path = ~/.common-gitconfig
+[user]
+  name = $NAME
+  email = $EMAIL
+EOF
+}
+
+configure_home() {
+  stow --dir="$SCRIPT_DIR" --target "$HOME" nvim tmux zsh git
+}
+
+main
