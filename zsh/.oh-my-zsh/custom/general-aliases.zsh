d8fcdebc0baa0eb05d474f3dea4259c0eccaa1cd
Georgi Dankov <gddankov@gmail.com>
2020-04-15 14:52:28 +0000

    Add alias to authenticate to cf
    Signed-off-by: Mario Nitchev <marionitchev@gmail.com>

--- zsh/.oh-my-zsh/custom/general-aliases.zsh
+++ zsh/.oh-my-zsh/custom/general-aliases.zsh
@@ -9,3 +9,22 @@ alias watch='watch '
 alias vim='nvim'
 alias pbcopy='xsel --clipboard --input'
 alias pbpaste='xsel --clipboard --output'
+alias cflogin='cf-lite'
+
+cf-lite() {
+  local password="$1"
+  local endpoint="$2"
+  if [ -z $endpoint ]; then
+    endpoint="https://api.$(cf-ip).nip.io"
+  fi
+  cf api $endpoint --skip-ssl-validation
+  cf login -u admin -p "$password"
+  cf create-org o
+  cf target -o o
+  cf create-space s
+  cf target -o o -s s
+}
+
+cf-ip() {
+  kubectl get nodes -ojsonpath="{ .items[0].metadata.labels['ibm-cloud\.kubernetes\.io/external-ip'] }"
+}
