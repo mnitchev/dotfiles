c07ef6d17190c03e455aab6065c4c0fddd250dd5
Georgi Sabev <georgethebeatle@gmail.com>
2020-04-23 12:47:02 +0000

    Delete old cluster configs when using ibmcluster
    Signed-off-by: Mario Nitchev <marionitchev@gmail.com>

--- zsh/.oh-my-zsh/custom/ibmcloud-aliases.zsh
+++ zsh/.oh-my-zsh/custom/ibmcloud-aliases.zsh
@@ -1,2 +1,11 @@
 alias ibmlogin='ibmcloud login -u eirini@cloudfoundry.org -p $(pass eirini/ibm-id)'
-alias ibmcluster='ibmcloud ks cluster config --cluster'
+alias ibmcluster='replace-ibm-cluster'
+
+replace-ibm-cluster() {
+    local name old_name
+    name="$1"
+    if old_name="$(kubectl config get-clusters | grep "$name")"; then
+        kubectl config delete-cluster "$old_name"
+    fi
+    ibmcloud ks cluster config --cluster "$name"
+}
