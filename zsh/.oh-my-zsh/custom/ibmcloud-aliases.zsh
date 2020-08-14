3fce240271160792ee4b026fe1faabc804c73f13
Mario Nitchev <marionitchev@gmail.com>
2020-08-14 09:35:14 +0000

    Set account id and --no-region in ibmlogin
    Co-authored-by: Danail Branekov <danailster@gmail.com>

--- zsh/.oh-my-zsh/custom/ibmcloud-aliases.zsh
+++ zsh/.oh-my-zsh/custom/ibmcloud-aliases.zsh
@@ -2,7 +2,8 @@ alias ibmlogin='ibm-login'
 alias ibmcluster='replace-ibm-cluster'
 
 ibm-login() {
-  ibmcloud login -u eirini@cloudfoundry.org -p $(pass eirini/ibm-id)
+  # Log into the Bluemix Flintstone account via the -c parameter
+  ibmcloud login -u eirini@cloudfoundry.org -p $(pass eirini/ibm-id) -c "7e51fbb83371a0cb0fd553fab15aebf4" --no-region
   eirinisay  "IMPORTANT NOTICE: Please DO NOT delete ibm clusters \"pebbles01\" and \"dino02\" as they are owned by the Quarks Team" | lolcat -as 200
 }
 
