b41287b33e82914008b43fd648f949248f6edd40
Anonymous Eirininaut <eirini@cloudfoundry.org>
2021-02-25 19:08:46 +0200

    Add useful gpg aliases
    Co-authored-by: Danail Branekov <danailster@gmail.com>

---
+++ zsh/.oh-my-zsh/custom/gpg-aliases.zsh
@@ -0,0 +1,38 @@
+alias fix-gpg='symlink-gpg-agent-socket'
+alias use-gpg='symlink-gpg-agent-socket-interactive'
+alias who-gpg='get-active-gpg-socket'
+
+gpg-socket-symlinked() {
+  local system_gpg_socket_location
+  system_gpg_socket_location="$(gpgconf --list-dir agent-socket)"
+
+  test -L "$system_gpg_socket_location"
+}
+
+symlink-gpg-agent-socket() {
+  local system_gpg_socket_location user
+  user="$1"
+  if [[ -z "$user" ]]; then
+    user="$(whoami)"
+  fi
+
+  if [[ "$user" == vagrant ]]; then
+    return
+  fi
+
+  system_gpg_socket_location="$(gpgconf --list-dir agent-socket)"
+  rm -f "$system_gpg_socket_location"
+  ln -s "${HOME}/.gnupg/S.gpg-agent-$user" "$system_gpg_socket_location"
+}
+
+symlink-gpg-agent-socket-interactive() {
+  local prefix user
+  prefix="S.gpg-agent-"
+
+  user=$(ls -1 "$HOME/.gnupg" | grep "$prefix" | sed -e "s/^$prefix//" | fzf)
+  symlink-gpg-agent-socket "$user"
+}
+
+get-active-gpg-socket() {
+  basename $(readlink $(gpgconf --list-dirs agent-socket)) | sed -e "s/^S.gpg-agent-//"
+}
--- zsh/.oh-my-zsh/custom/ssh-aliases.zsh
+++ zsh/.oh-my-zsh/custom/ssh-aliases.zsh
@@ -1,6 +1,4 @@
 alias fix-ssh='export-ssh-agent-config'
-alias fix-gpg='symlink-gpg-agent-socket'
-alias fix-user='fix-ssh && fix-gpg'
 alias fixnload='fix-ssh && load-key'
 
 export-ssh-agent-config() {
@@ -18,25 +16,3 @@ ssh-agent-socket-available() {
   test -S "$SSH_AUTH_SOCK"
 }
 
-gpg-socket-symlinked() {
-  local system_gpg_socket_location
-  system_gpg_socket_location="$(gpgconf --list-dir agent-socket)"
-
-  test -L "$system_gpg_socket_location"
-}
-
-symlink-gpg-agent-socket() {
-  local system_gpg_socket_location user
-  user="$1"
-  if [[ -z "$user" ]]; then
-    user="$(whoami)"
-  fi
-
-  if [[ "$user" == vagrant ]]; then
-    return
-  fi
-
-  system_gpg_socket_location="$(gpgconf --list-dir agent-socket)"
-  rm -f "$system_gpg_socket_location"
-  ln -s "${HOME}/.gnupg/S.gpg-agent-$user" "$system_gpg_socket_location"
-}
