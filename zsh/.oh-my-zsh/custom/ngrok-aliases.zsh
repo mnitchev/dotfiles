c0f05be2a77795be0258216fc6cf2ff596908ead
Mario Nitchev <marionitchev@gmail.com>
2021-02-26 13:55:46 +0000

    Make pssh work with ngrok
    [#176778878]
    
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- zsh/.oh-my-zsh/custom/ngrok-aliases.zsh
+++ zsh/.oh-my-zsh/custom/ngrok-aliases.zsh
@@ -10,26 +10,36 @@ yellow="\033[33m"
 
 print-ssh-command() {
   username=$(whoami)
-  ip=$(curl -s ipecho.net/plain)
+  connection="$(get-ngrok-host)"
+  if [[ "$?" -ne 0 ]]; then
+    ip=$(curl -s ipecho.net/plain)
+    connection="$username@$ip"
+  fi
+
   echo "${bold}${yellow}Using plain ssh:${normal}"
   echo
   echo "ssh $username@$ip"
   echo
   echo "${bold}${yellow}Or using pair-connect:${normal}"
   echo
-  echo "pair-connect -a $username@$ip"
+  echo "pair-connect -a $connection"
 }
 
 print-ngrok-tunnel() {
+  echo "ssh $(get-ngrok-host)"
+}
+
+get-ngrok-host() {
 url=$(curl http://localhost:4040/api/tunnels 2>/dev/null | jq -r '.tunnels[0].public_url')
 
 if [[ "$url" == "" ]] || [[ "$url" == "null" ]]; then
   echo "No tunnels running?"
+  return 1
 else
   host="$(echo "$url" | cut -d / -f 3 | cut -d : -f 1)"
   port="$(echo "$url" | cut -d / -f 3 | cut -d : -f 2)"
 
-  echo "ssh -p $port $(whoami)@$host"
+  echo "$(whoami)@$host -p $port"
 fi
 }
 
