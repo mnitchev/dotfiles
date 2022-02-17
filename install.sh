1f24999c00a6e6d38108c4c9804cc0f8e50da990
Mario Nitchev <marionitchev@gmail.com>
2022-02-17 23:24:45 +0200

    remove unwanted tools

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 main() {
   configure_home "$@"
   generate_gitconfig
-  compile_authorized_keys
-  compile_gpg_keys
   prepare_coq_nvim
 }
 
@@ -20,51 +18,13 @@ generate_gitconfig() {
 [include]
   path = ~/.common-gitconfig
 [user]
-  name = "Anonymous Eirininaut"
-  email = "eirini@cloudfoundry.org"
+  name = "Mario Nitchev"
+  email = "marionitchev@gmail.com"
 [duet "env"]
-  git-author-initials = ae
-  git-author-name = Anonymous Eirininaut
-  git-author-email = eirini@cloudfoundry.org
+  git-author-initials = mn
+  git-author-name = Mario Nitchev
+  git-author-email = marionitchev@gmail.com
 EOF
-}
-
-compile_authorized_keys() {
-  local authorized_keys keys key
-  authorized_keys="$HOME/.ssh/authorized_keys"
-
-  for gh_name in $(awk '{ if (usernames==1) print $2 }; /^usernames:/ { usernames=1 }' "$HOME/.git-authors"); do
-    keys=$(curl -sL "https://api.github.com/users/$gh_name/keys")
-    if [[ "$keys" =~ "rate limit exceeded" ]]; then
-      echo "+-------------------------------------------------------------------------+"
-      echo "| ⚠️  WARNING:                                                             |"
-      echo "+-------------------------------------------------------------------------+"
-      echo "| Looks like we have exceeded the github rate limit. Skipping generation  |"
-      echo "| of authorized_keys file. You will have to manually add authorized keys! |"
-      echo "+-------------------------------------------------------------------------+"
-      return
-    fi
-    key=$(jq -r ".[].key" <<<"$keys")
-    echo "$key $gh_name" >>"$HOME/.ssh/authorized_keys"
-  done
-
-  # remove duplicate keys
-  sort --unique "$authorized_keys" -o "$authorized_keys"
-}
-
-compile_gpg_keys() {
-  pushd "${HOME}/workspace/eirini-private-config/pass/eirini" >/dev/null || exit 1
-  {
-    cat .gpg-id | while read email; do
-      if gpg --list-keys "$email" >/dev/null; then
-        continue
-      fi
-
-      echo -e "1\n" | gpg --no-tty --command-fd 0 --keyserver keys.openpgp.org --search-keys $email
-      echo -e "trust\n5\ny\nsave\n" | gpg --no-tty --command-fd 0 --edit-key $email
-    done
-  }
-  popd >/dev/null || exit 1
 }
 
 configure_home() {
