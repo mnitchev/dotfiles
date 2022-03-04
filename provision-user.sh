6749139280114bb82294d96893e7b407ac6b03ab
Mario Nitchev <marionitchev@gmail.com>
2022-03-04 09:43:40 +0200

    Add provisioning scripts

---
+++ provision-user.sh
@@ -0,0 +1,210 @@
+#!/bin/bash
+set -euo pipefail
+
+readonly USAGE="Usage: provision.sh [-l | -c <command_name>]"
+
+main() {
+  while getopts ":lch" opt; do
+    case ${opt} in
+      l)
+        declare -F | awk '{ print $3 }' | grep -vE "(main|go_install|git_clone)"
+        exit 0
+        ;;
+      c)
+        shift $((OPTIND - 1))
+        for command in $@; do
+          $command
+        done
+        exit 0
+        ;;
+      h)
+        echo $USAGE
+        exit 0
+        ;;
+      \?)
+        echo "Invalid option: $OPTARG" 1>&2
+        echo $USAGE
+        exit 1
+        ;;
+    esac
+  done
+  shift $((OPTIND - 1))
+  echo ">>> Installing everything..."
+  mkdir_home_user_bin
+  install_gotools
+  install_docker
+  install_ohmyzsh
+  install_vim_plug
+  install_nvim_extensions
+  install_cred_alert
+  configure_dotfiles
+  install_vim_plugins
+  install_misc_tools
+  install_pure_zsh_theme
+  install_tmux_plugin_manager
+  install_zsh_autosuggestions
+  switch_to_zsh
+}
+
+mkdir_home_user_bin() {
+  mkdir -p $HOME/bin
+}
+
+install_cred_alert() {
+  os_name=$(uname | awk '{print tolower($1)}')
+  curl -o cred-alert-cli \
+    https://github.com/pivotal-cf/cred-alert/releases/download/2021-06-09-20-24-53/cred-alert-cli_linux
+  chmod 755 cred-alert-cli
+  mv cred-alert-cli "$HOME/bin/"
+}
+
+install_docker() {
+  echo ">>> Installing Docker"
+  if command -v docker; then
+    sudo apt upgrade docker-ce docker-ce-cli docker-ce-rootless-extras -y
+  else
+    curl -fsSL get.docker.com | sudo sh
+    sudo usermod -aG docker $USER
+  fi
+}
+
+install_gotools() {
+  echo ">>> Installing golangci-lint"
+  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$HOME/go/bin/" v1.44.0
+
+  echo ">>> Installing gopls"
+  go_install golang.org/x/tools/gopls
+
+  echo ">>> Installing fillstruct"
+  go_install github.com/davidrjenni/reftools/cmd/fillstruct
+
+}
+
+install_ohmyzsh() {
+  echo ">>> Installing Oh My Zsh"
+  [ ! -d "$HOME/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  # Delete default .zshrc to avoid stow conflicts
+  rm -f "$HOME/.zshrc"
+}
+
+install_tmux_plugin_manager() {
+  echo ">>> Installing TPM"
+  git_clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+}
+
+install_zsh_autosuggestions() {
+  echo ">>> Installing zsh-autosuggestions"
+  git_clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+}
+
+install_vim_plug() {
+  echo ">>> Installing vim-plug"
+  curl -fLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+}
+
+install_nvim_extensions() {
+  echo ">>> Installing the NeoVim extensions"
+  sudo npm install -g neovim
+  pip3 install --upgrade pip
+  pip3 install --upgrade neovim
+}
+
+git_clone() {
+  local url path name branch
+  url=$1
+  path=${2:-""}
+  branch=${3:-""}
+
+  if [ -z "$path" ]; then
+    name=$(echo "$url" | sed 's/\.git//g' | cut -d / -f 2)
+    path="$HOME/workspace/$name"
+  fi
+
+  if [ -d "$path" ]; then
+    echo "Repository $path already exists. Skipping git clone..."
+    return
+  fi
+
+  git clone "$url" "$path"
+
+  if [ -f "$path/.gitmodules" ]; then
+    git -C "$path" submodule update --init --recursive
+  fi
+
+  if [ -n "$branch" ]; then
+    git -C "$path" switch "$branch"
+  fi
+}
+
+configure_dotfiles() {
+  echo ">>> Installing dotfiles"
+
+  git_clone "git@github.com:mnitchev/dotfiles.git"
+
+  pushd "$HOME/workspace/dotfiles"
+  {
+    git checkout master
+    git pull -r
+    ./install.sh
+
+    export GIT_DUET_CO_AUTHORED_BY=1
+    export GIT_DUET_GLOBAL=true
+    git solo mn # initialise git-duet
+    git init    # install git-duet hooks on dotfiles
+  }
+  popd
+}
+
+install_vim_plugins() {
+  echo ">>> Installing the NeoVim plugins"
+  # If we run the command below with init.vim (the default) it will fail because some lua files
+  # cannot be loaded. This is so because plug hasn't yet installed the corresponding plugins that
+  # are bringing those files (dependency loop). In order to untie the loop we do plug install/update
+  # with just the plug part of the config. Later when you run nvim it will laod and install all remaining
+  # stuff without you having to run :PlugInstall.
+  nvim -u "$HOME/.config/nvim/plug.vim" --headless +PlugClean +PlugInstall +PlugUpdate +UpdateRemotePlugins +qall
+}
+
+install_misc_tools() {
+  echo ">>> Installing Ginkgo"
+  go_install "github.com/onsi/ginkgo/ginkgo"
+
+  echo ">>> Installing k9s (v0.25.8)"
+  curl -L https://github.com/derailed/k9s/releases/download/v0.25.8/k9s_Linux_x86_64.tar.gz | tar xvzf - -C "$HOME/bin" k9s
+
+  echo ">>> Installing kind (v0.11.1)"
+  curl -L https://github.com/kubernetes-sigs/kind/releases/download/v0.11.1/kind-linux-amd64 -o "$HOME/bin/kind"
+  chmod +x "$HOME/bin/kind"
+}
+
+go_install() {
+  local package=$1
+  local version=${2:-latest}
+
+  /usr/local/go/bin/go install $package@$version
+}
+
+install_pure_zsh_theme() {
+  echo ">>> Installing the pure prompt"
+  mkdir -p "$HOME/.zsh"
+  git_clone "https://github.com/sindresorhus/pure.git" "$HOME/.zsh/pure"
+  pushd "$HOME/.zsh/pure"
+  {
+    # pure have switched from `master` to `main` for their main branch
+    # TODO remove this once everyone has been migrated
+    if git show-ref --quiet refs/heads/master; then
+      git branch -m master main
+      git branch --set-upstream-to=origin/main
+    fi
+    git pull -r
+  }
+  popd
+}
+
+switch_to_zsh() {
+  echo ">>> Setting Zsh as the default shell"
+  sudo chsh -s /bin/zsh "$(whoami)"
+}
+
+main $@
---
+++ provision.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+set -euo pipefail
+
+readonly USAGE="Usage: provision.sh [-l | -c <command_name>]"
+
+main() {
+  if [[ $EUID -ne 0 ]]; then
+    echo "Script must be run as root."
+    exit 1
+  fi
+
+  while getopts ":lch" opt; do
+    case ${opt} in
+      l)
+        declare -F | awk '{ print $3 }' | grep -v main
+        exit 0
+        ;;
+      c)
+        shift $((OPTIND - 1))
+        for command in $@; do
+          $command
+        done
+        exit $?
+        ;;
+      h)
+        echo $USAGE
+        exit 0
+        ;;
+      \?)
+        echo "Invalid option: $OPTARG" 1>&2
+        echo $USAGE
+        exit 1
+        ;;
+    esac
+  done
+  shift $((OPTIND - 1))
+  echo ">>> Installing everything..."
+
+  setup_locale
+  install_packages
+  install_snaps
+  install_kubectl
+  install_nodejs
+  install_nvim
+  install_npm_packages
+  install_golang
+  install_misc_tools
+  install_delta
+  install_github_cli
+  install_helm3
+}
+
+setup_locale() {
+  echo ">>> Setting up the en_US locale"
+  apt-get -y install locales
+  locale-gen en_US.UTF-8
+  update-locale LANG=en_US.UTF-8
+}
+
+install_packages() {
+  echo ">>> Installing the APT packages"
+  apt-get update
+  apt-get -y install \
+    apt-transport-https \
+    autoconf \
+    automake \
+    build-essential \
+    ca-certificates \
+    cmake \
+    cowsay \
+    curl \
+    direnv \
+    fd-find \
+    fortune \
+    fzf \
+    g++ \
+    git \
+    gnome-tweaks \
+    iputils-ping \
+    jq \
+    lastpass-cli \
+    libdevmapper-dev \
+    libevent-dev \
+    libncurses5-dev \
+    libssl-dev \
+    libtool \
+    libtool-bin \
+    net-tools \
+    ntp \
+    openvpn3 \
+    pkg-config \
+    python3 \
+    python3-dev \
+    python3-pip \
+    python3-venv \
+    ripgrep \
+    slack-desktop \
+    software-properties-common \
+    stow \
+    tmux \
+    unzip \
+    wget \
+    xsel \
+    zsh
+}
+
+install_snaps() {
+  echo ">>> Installing the Snap packages"
+  snap install shfmt
+  snap install lolcat
+  snap install shellcheck --edge
+}
+
+install_kubectl() {
+  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+  rm -f kubectl
+}
+
+install_golang() {
+  echo ">>> Installing Golang"
+  rm -rf /usr/local/go
+  mkdir -p /usr/local/go
+  curl -sL "https://dl.google.com/go/go1.17.5.linux-amd64.tar.gz" | tar xz -C "/usr/local"
+}
+
+install_nodejs() {
+  echo ">>> Installing NodeJS"
+  curl -sL https://deb.nodesource.com/setup_14.x | bash -
+  apt-get -y install nodejs
+}
+
+install_nvim() {
+  echo ">>> Installing NeoVim"
+  if grep -q '^deb http://ppa.launchpad.net/neovim-ppa/stable/ubuntu' /etc/apt/sources.list.d/*.list; then
+    add-apt-repository --remove ppa:neovim-ppa/stable -y
+    apt-get remove -y neovim
+  fi
+
+  url="$(curl -s https://api.github.com/repos/neovim/neovim/releases/tags/v0.6.1 | jq -r '.assets[] | select(.name == "nvim.appimage") | .browser_download_url')"
+
+  curl -sL "$url" --output /tmp/nvim
+  chmod +x /tmp/nvim
+  mv /tmp/nvim /usr/bin/
+}
+
+install_misc_tools() {
+  echo ">>> Installing git-duet"
+  curl -sL "https://github.com/git-duet/git-duet/releases/download/0.7.0/linux_amd64.tar.gz" | tar xvz -C /usr/bin
+
+  echo ">>> Installing terraform"
+  curl -sL "https://releases.hashicorp.com/terraform/0.15.4/terraform_0.15.4_linux_amd64.zip" -o /tmp/terraform.zip
+  unzip -ou /tmp/terraform.zip -d /usr/bin
+  rm /tmp/terraform.zip
+
+  echo ">>> Installing yq"
+  curl -sLo yq https://github.com/mikefarah/yq/releases/download/v4.6.2/yq_linux_amd64 && install yq /usr/local/bin/ && rm -f yq
+}
+
+install_npm_packages() {
+  echo ">>> Installing npm packages"
+  npm install -g bash-language-server tldr
+}
+
+install_delta() {
+  echo ">>> Installing delta"
+  set -x
+  curl -sL https://github.com/dandavison/delta/releases/download/0.1.1/delta-0.1.1-x86_64-unknown-linux-musl.tar.gz -o /tmp/delta.tar.gz
+  tar xzvf /tmp/delta.tar.gz
+  mv delta-0.1.1-x86_64-unknown-linux-musl/delta /usr/bin
+  rm -fr delta-0.1.1-x86_64-unknown-linux-musl /tmp/delta.tar.gz
+  set +x
+}
+
+install_github_cli() {
+  echo ">>> Installing Github CLI"
+  apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+  apt-add-repository https://cli.github.com/packages
+  apt update
+  apt install gh
+}
+
+install_helm3() {
+  echo ">>> Installing Helm3"
+  curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 &&
+    chmod 700 get_helm.sh &&
+    ./get_helm.sh &&
+    rm -f ./get_helm.sh
+}
+
+main $@
