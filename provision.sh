3b72e7c70091dd1e3c3864fc1ecab6ddff98c9f3
Mario Nitchev <marionitchev@gmail.com>
2022-03-29 15:47:14 +0300

    Install sshd, delta, gcloud, vault, various packages
    Also add keyboard layout for XPS 15 + Ducky One 2 mini

--- provision.sh
+++ provision.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 readonly USAGE="Usage: provision.sh [-l | -c <command_name>]"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 main() {
   if [[ $EUID -ne 0 ]]; then
@@ -36,6 +37,7 @@ main() {
   shift $((OPTIND - 1))
   echo ">>> Installing everything..."
 
+  add_sshd_config
   setup_locale
   install_packages
   install_snaps
@@ -49,6 +51,14 @@ main() {
   install_delta
   install_github_cli
   install_helm3
+  install_layout
+  install_gcloud
+  install_dive
+}
+
+add_sshd_config() {
+  echo "StreamLocalBindUnlink yes" >/etc/ssh/sshd_config.d/streamlocalbindunlink.conf
+  systemctl restart ssh.service
 }
 
 setup_locale() {
@@ -67,6 +77,8 @@ install_packages() {
     automake \
     build-essential \
     ca-certificates \
+    ca-certificates \
+    cargo \
     cmake \
     cowsay \
     curl \
@@ -77,24 +89,29 @@ install_packages() {
     g++ \
     git \
     gnome-tweaks \
+    gnupg \
     iputils-ping \
     jq \
     lastpass-cli \
     libdevmapper-dev \
     libevent-dev \
+    libfontconfig1-dev \
+    libfreetype6-dev \
     libncurses5-dev \
     libssl-dev \
     libtool \
     libtool-bin \
+    libxcb-xfixes0-dev \
+    libxkbcommon-dev \
     net-tools \
     ntp \
+    openssh-server \
     pkg-config \
     python3 \
     python3-dev \
     python3-pip \
     python3-venv \
     ripgrep \
-    slack \
     software-properties-common \
     stow \
     tmux \
@@ -105,7 +122,7 @@ install_packages() {
 }
 
 install_openvpn() {
-  curl -fsSL https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/openvpn-repo-pkg-keyring.gpg
+  curl -fsSL https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub | gpg --dearmor >/etc/apt/trusted.gpg.d/openvpn-repo-pkg-keyring.gpg
   curl -fsSL https://swupdate.openvpn.net/community/openvpn3/repos/openvpn3-impish.list >/etc/apt/sources.list.d/openvpn3.list
   apt update
   apt-get install -y openvpn3
@@ -152,6 +169,9 @@ install_nvim() {
 }
 
 install_misc_tools() {
+  echo ">>> Installing ngrok"
+  curl -sL "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz" | tar xz -C /usr/bin
+
   echo ">>> Installing git-duet"
   curl -sL "https://github.com/git-duet/git-duet/releases/download/0.7.0/linux_amd64.tar.gz" | tar xvz -C /usr/bin
 
@@ -172,10 +192,9 @@ install_npm_packages() {
 install_delta() {
   echo ">>> Installing delta"
   set -x
-  curl -sL https://github.com/dandavison/delta/releases/download/0.1.1/delta-0.1.1-x86_64-unknown-linux-musl.tar.gz -o /tmp/delta.tar.gz
-  tar xzvf /tmp/delta.tar.gz
-  mv delta-0.1.1-x86_64-unknown-linux-musl/delta /usr/bin
-  rm -fr delta-0.1.1-x86_64-unknown-linux-musl /tmp/delta.tar.gz
+  curl -sL https://github.com/dandavison/delta/releases/download/0.12.1/git-delta_0.12.1_amd64.deb -o /tmp/delta.deb
+  dpkg -i /tmp/delta.deb
+  rm -f /tmp/delta.deb
   set +x
 }
 
@@ -193,6 +212,33 @@ install_helm3() {
     chmod 700 get_helm.sh &&
     ./get_helm.sh &&
     rm -f ./get_helm.sh
+}
+
+install_layout() {
+  cp $SCRIPT_DIR/xkb/xy /usr/share/X11/xkb/symbols/xy
+  if ! grep -q '<name>xy</name>' /usr/share/X11/xkb/rules/evdev.xml; then
+    sed -i '/<layoutList>/r xkb/layout.xml' /usr/share/X11/xkb/rules/evdev.xml
+    dpkg-reconfigure xkb-data
+    gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'xy'), ('xkb', 'us'), ('xkb', 'bg+phonetic')]"
+
+  fi
+}
+
+install_vault() {
+  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+  sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+  sudo apt-get update && sudo apt-get install vault
+}
+
+install_gcloud() {
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+  sudo apt-get update && sudo apt-get install google-cloud-cli
+}
+
+install_dive() {
+  wget https://github.com/wagoodman/dive/releases/download/v0.9.2/dive_0.9.2_linux_amd64.deb
+  sudo apt install ./dive_0.9.2_linux_amd64.deb
 }
 
 main $@
