c115d07515be9948e5d3905e2245531b9d5f456a
Mario Nitchev <marionitchev@gmail.com>
2022-03-29 15:54:09 +0300

    Remove dive .deb file after installing

--- provision.sh
+++ provision.sh
@@ -238,7 +238,8 @@ install_gcloud() {
 
 install_dive() {
   wget https://github.com/wagoodman/dive/releases/download/v0.9.2/dive_0.9.2_linux_amd64.deb
-  sudo apt install ./dive_0.9.2_linux_amd64.deb
+  apt install ./dive_0.9.2_linux_amd64.deb
+  rm ./dive_0.9.2_linux_amd64.deb
 }
 
 main $@
