cb2638c199d304af744458fc1054a3e4c92f2770
Mario Nitchev <marionitchev@gmail.com>
2025-03-12 11:15:02 +0200

    fix ln for real now

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ configure_home() {
 
 configure_nvim() {
   git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
-  ln -s custom.lua ~/.config/nvim/lua/plugins/custom.lua
+  ln -s $PWD/custom.lua ~/.config/nvim/lua/plugins/custom.lua
 
 }
 
